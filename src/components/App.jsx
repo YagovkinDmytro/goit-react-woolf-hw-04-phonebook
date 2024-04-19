b3047662923299426b53de47ff5023450579e45e
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-04-19 15:42:36 +0200

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PhonebookForm from './PhonebookForm/PhonebookForm';
 import Section from './Section/Section';
